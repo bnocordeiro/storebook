1fda642fb7daec27cefc6c5c70d4489e93da8b65
Bruno Dutra <brunocordeiro.edf@gmail.com>
2024-02-22 16:10:33 -0300

    New build.yml for Github Actions

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -1,4 +1,3 @@
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from product.models import Product
@@ -6,9 +5,8 @@ from product.serializers.product_serializer import ProductSerializer
 
 
 class ProductViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticated]
     serializer_class = ProductSerializer
 
     def get_queryset(self):
         return Product.objects.all().order_by('id')
-    
+
